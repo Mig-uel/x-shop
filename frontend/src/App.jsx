56e8e8974c9fd3c84433a986fe65609858d30b9f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-07 17:52:17 -0400

    Add header and footer components to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/header.component'
+import Footer from './components/footer.component'
+
 function App() {
-  return <></>
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to X-Shop</h1>
+        </Container>
+        <Footer />
+      </main>
+    </>
+  )
 }
 
 export default App
