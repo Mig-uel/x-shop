0b1bed75ccf972010468831e71fc5766552f61f0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-07 18:55:13 -0400

    Add Product component to home screen

--- frontend/src/screens/home.screen.jsx
+++ frontend/src/screens/home.screen.jsx
@@ -1,5 +1,6 @@
 import { Row, Col } from 'react-bootstrap'
 import products from '../products'
+import Product from '../components/product.component'
 
 const HomeScreen = () => {
   return (
@@ -9,7 +10,7 @@ const HomeScreen = () => {
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <h3>{product.name}</h3>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
