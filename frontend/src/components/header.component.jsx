e6f645920013489a51662929f050a7f1772405c0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-07 17:36:23 -0400

    feat: Add header component
    Add a new header component to the frontend. This component will be responsible for displaying the navigation bar with icons for shopping cart and user profile. The component is currently empty and will be further developed in subsequent commits.

---
+++ frontend/src/components/header.component.jsx
@@ -0,0 +1,8 @@
+import { navbar, Nav, Container } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+  return <></>
+}
+
+export default Header
