4663d5abe6f498ea920f32008174ec1e6f6f0c33
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-07 19:06:08 -0400

    Refactor product component to use react-router-dom for navigation

--- frontend/src/components/product.component.jsx
+++ frontend/src/components/product.component.jsx
@@ -1,18 +1,19 @@
 import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Product = ({ product: { _id, image, name, price } }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${_id}`}>
+      <Link to={`/product/${_id}`}>
         <Card.Img src={image} variant='top' />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${_id}`}>
+        <Link to={`/product/${_id}`}>
           <Card.Title as='div'>
             <strong>{name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as='h3'>${price}</Card.Text>
       </Card.Body>
