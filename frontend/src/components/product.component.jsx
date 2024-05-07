ee4243ec3dcb1049d518fff4b8b112c5922c1b2b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-07 19:28:03 -0400

    Add rating component to product display

--- frontend/src/components/product.component.jsx
+++ frontend/src/components/product.component.jsx
@@ -1,7 +1,10 @@
+import Rating from './rating.component'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-const Product = ({ product: { _id, image, name, price } }) => {
+const Product = ({
+  product: { _id, image, name, price, rating, numReviews },
+}) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${_id}`}>
@@ -10,11 +13,13 @@ const Product = ({ product: { _id, image, name, price } }) => {
 
       <Card.Body>
         <Link to={`/product/${_id}`}>
-          <Card.Title as='div'>
+          <Card.Title as='div' className='product-title'>
             <strong>{name}</strong>
           </Card.Title>
         </Link>
-
+        <Card.Text as='div'>
+          <Rating value={rating} text={`${numReviews} reviews`} />
+        </Card.Text>
         <Card.Text as='h3'>${price}</Card.Text>
       </Card.Body>
     </Card>
