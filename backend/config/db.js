a4fd3c593c7f781fdea70dbbfb11c6d9ab82d02f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-08 14:58:23 -0400

    refactor: Update MongoDB connection logging in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MONGODB CONNECTED: ${conn.connection.name}`.blue)
+    console.log(`MONGODB CONNECTED: ${conn.connection.name}`.blue.inverse)
   } catch (error) {
     console.log(error)
     process.exit(1)
