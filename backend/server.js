4d3922ec9d3fcb38eac5428b5f0d6afe9935ba68
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-07 23:10:54 -0400

    chore: Add backend server.js file

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const colors = require('colors')
+
+const port = 5000
+const app = express()
+
+app.get('/', (req, res) => {})
+
+app.listen(port, () => console.log(`SERVER STARTED ON PORT: ${port}`))
