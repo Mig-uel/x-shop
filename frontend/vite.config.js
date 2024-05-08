d5e9b3fd3e9ddb3d655216636479a116fffd6ace
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-08 00:28:33 -0400

    chore: Update vite.config.js with eslintPlugin for linting JavaScript and JSX files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),
+  ],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
+      '/api': 'http://localhost:5000',
+
+      '/uploads': 'http://localhost:5000',
     },
   },
 })
