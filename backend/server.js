80e5ecb33d0aaaed2b810df3ebcf075c843798be
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-07 23:31:39 -0400

    chore: Update backend server.js to include API endpoints for retrieving products

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const colors = require('colors')
+const products = require('./data')
 
 const port = 5000
 const app = express()
 
-app.get('/', (req, res) => {})
+app.get('/', (req, res) => res.send('Hi'))
+app.get('/api/products', (req, res) => res.json(products))
+app.get('/api/products/:id', (req, res) => {
+  const { id } = req.params
+  const product = products.find((p) => p._id === id)
+
+  res.status(200).json(product)
+})
 
 app.listen(port, () => console.log(`SERVER STARTED ON PORT: ${port}`.magenta))
