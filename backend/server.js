631ec63c8afc5ce29f42f4363c3490f4188ebf91
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-09 18:18:05 -0400

    refactor: Update server.js to use custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,20 @@ const connectDB = require('./config/db')
 // router
 const productRoutes = require('./routes/product.routes')
 
+// middleware
+const { notFound, errorHandler } = require('./middleware/error.middleware')
+
 dotenv.config()
 const port = process.env.PORT || 5000
 connectDB() // start mongodb connection
 const app = express() // init express obj
 
+// main api route
 app.use('/api/products', productRoutes)
 
-// TODO: Create custom async error handler
-// custom async error handler
-// app.use((error, req, res, next) => {
-//   res.status(500).json({ error: error.message })
-// })
+// custom error handler
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () =>
   console.log(`SERVER STARTED ON PORT: ${port}`.green.inverse)
