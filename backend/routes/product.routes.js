129408398cbeb5f8d269cf0a9b4adc0d64565573
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-09 16:12:10 -0400

    refactor: Add API endpoints for retrieving products and product details

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const products = require('../data/data')
 
 const router = express.Router()
+
+router.get('/', (req, res) => res.json(products))
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const product = products.find((p) => p._id === id)
+
+  res.status(200).json(product)
+})
 
 module.exports = router
