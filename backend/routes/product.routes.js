650abc3524e989388a71b479af969a4209c2a669
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-09 17:47:36 -0400

    refactor: Update product.routes.js to use asyncHandler middleware for handling asynchronous functions

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const asyncHandler = require('../middleware/asyncHandler.middleware')
+const Product = require('../models/product.model')
 
 const router = express.Router()
 
 router.get(
   '/',
-  asyncHandler(async (req, res) => res.status(200).json(products))
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+
+    res.status(200).json(products)
+  })
 )
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const { id } = req.params
-    const product = products.find((p) => p._id === id)
+    const product = await Product.findById(id)
 
     res.status(200).json(product)
   })
