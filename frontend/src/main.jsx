da70452e3a172278f3fca268a5485784d858286f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-10 15:54:47 -0400

    refactor: Add react-redux provider to main.jsx for global state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Link,
   Route,
   RouterProvider,
 } from 'react-router-dom'
 import App from './App.jsx'
+
+// global state
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 
 // styles
 import './assets/styles/index.css'
@@ -34,6 +37,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
