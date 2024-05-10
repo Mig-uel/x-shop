9d4298fc2a02d4561fb2826efcd5f1f4325d5ebb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-10 17:27:35 -0400

    refactor: Update home screen to use Redux API integration

--- frontend/src/screens/home.screen.jsx
+++ frontend/src/screens/home.screen.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { useGetProductsQuery } from '../store/api/productsEndpoints.api'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/product.component'
 
 const HomeScreen = () => {
-  const data = useLoaderData()
-  const [products, setProducts] = useState(data)
+  const { data: products, isLoading, error } = useGetProductsQuery()
 
   return (
     <>
       <h1>Latest Products</h1>
 
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error?.data?.message || error?.error}</p>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
