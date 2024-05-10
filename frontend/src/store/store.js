790b170b3725588fd8d2386e3c329c07874f5eea
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-10 16:48:44 -0400

    refactor: Update store.js to include apiSlice for Redux API integration

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+// api slice
+import { apiSlice } from './slice/api/api.slice'
+
 export default configureStore({
-  reducer: {},
+  reducer: {
+    // api reducer
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+
+  devTools: import.meta.env.MODE === 'development',
 })
