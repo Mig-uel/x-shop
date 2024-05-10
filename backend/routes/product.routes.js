8e5d224cf07af06ae2bba289ecf3beddc25ec14e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-09 20:10:48 -0400

    refactor: Update product.routes.js to use separate controllers for handling product requests

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,28 +1,15 @@
 const express = require('express')
-const asyncHandler = require('../middleware/asyncHandler.middleware')
-const Product = require('../models/product.model')
 
+// controller
+const {
+  getProducts,
+  getSingleProduct,
+} = require('../controllers/product/get.product.controller')
+
+// init router object
 const router = express.Router()
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-
-    res.status(200).json(products)
-  })
-)
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const { id } = req.params
-    const product = await Product.findById(id)
-
-    if (product) return res.status(200).json(product)
-
-    res.status(404)
-    throw new Error('Product not found')
-  })
-)
+router.get('/', getProducts)
+router.get('/:id', getSingleProduct)
 
 module.exports = router
