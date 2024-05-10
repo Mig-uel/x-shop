50e64c72acd5b28a7b5198b94c30a0a47479e411
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-09 20:14:20 -0400

    refactor: Update get.product.controller.js to use asyncHandler middleware for handling asynchronous functions

--- backend/controllers/product/get.product.controller.js
+++ backend/controllers/product/get.product.controller.js
@@ -1,12 +1,23 @@
 const Product = require('../../models/product.model')
 const asyncHandler = require('../../middleware/asyncHandler.middleware')
 
+/**
+ * @desc    Get all products
+ * @route   GET /api/products
+ * @access  Public
+ */
+
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({})
 
   res.status(200).json(products)
 })
 
+/**
+ * @desc    Get a single product
+ * @route   GET /api/products/:id
+ * @access  Public
+ */
 const getSingleProduct = asyncHandler(async (req, res) => {
   const { id } = req.params
   const product = await Product.findById(id)
