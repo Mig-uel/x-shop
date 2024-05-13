e8ca91e9eefae703c8267d628c5316b4a8871472
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-13 19:28:16 -0400

    refactor: Add cartSliceReducer to store configuration

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,8 +3,12 @@ import { configureStore } from '@reduxjs/toolkit'
 // API Slice
 import { apiSlice } from './api/apiSlice'
 
+// Cart Slice
+import cartSliceReducer from './cart/cartSlice'
+
 export default configureStore({
   reducer: {
+    cart: cartSliceReducer,
     // api reducer
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
