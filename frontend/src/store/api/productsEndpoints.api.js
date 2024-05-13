997d0ac6c8a527f5216a8e2a72691d035b96746e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-13 18:36:17 -0400

    refactor: Add getProductById query to productsEndpoints.api.js

--- frontend/src/store/api/productsEndpoints.api.js
+++ frontend/src/store/api/productsEndpoints.api.js
@@ -7,7 +7,12 @@ export const productsEndpoints = apiSlice.injectEndpoints({
       query: () => PRODUCTS_URL,
       keepUnusedDataFor: 5,
     }),
+    getProductById: builder.query({
+      query: (id) => `${PRODUCTS_URL}${id}`,
+      providesTags: ['Product'],
+      keepUnusedDataFor: 5,
+    }),
   }),
 })
 
-export const { useGetProductsQuery } = productsEndpoints
+export const { useGetProductsQuery, useGetProductByIdQuery } = productsEndpoints
