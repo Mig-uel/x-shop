86adc749717900900a20ee75254b9d845a2a167f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-14 18:31:40 -0400

    refactor: Update cart utils to calculate and update cart prices

---
+++ frontend/src/utils/cart.utils.js
@@ -0,0 +1,26 @@
+const addDecimals = (num) => (Math.round(num * 100) / 100).toFixed(2)
+
+export const updateCart = (state) => {
+  // Calculate items price (If order > $100, free ship. Else, $10)
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  )
+
+  // Calculate shipping price
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+
+  // Calculate tax price (15%)
+  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
+
+  // Calculate total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2)
+
+  // Save to localStorage
+  localStorage.setItem('cart', JSON.stringify(state))
+
+  return state
+}
