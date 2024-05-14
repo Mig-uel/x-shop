6340d0da6dbe2c20e6607afd20551520c0b96d5e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-13 20:54:18 -0400

    refactor: Optimize import statements in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from 'react-router-dom'
+
+// UI Components
 import { Container } from 'react-bootstrap'
 import Header from './components/header.component'
 import Footer from './components/footer.component'
-import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
