914a5a9b55f4bf0ee5a0b35ea7f73f3e1e6e9775
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-14 18:31:32 -0400

    refactor: Update cartSlice.js to use updateCart utility function

--- frontend/src/store/cart/cartSlice.js
+++ frontend/src/store/cart/cartSlice.js
@@ -1,10 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit/react'
+import { updateCart } from '../../utils/cart.utils'
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] }
-
-const addDecimals = (num) => (Math.round(num * 100) / 100).toFixed(2)
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -23,26 +22,7 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item]
       }
 
-      // Calculate items price (If order > $100, free ship. Else, $10)
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      )
-
-      // Calculate shipping price
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
-
-      // Calculate tax price (15%)
-      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
-
-      // Calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2)
-
-      // Save to localStorage
-      localStorage.setItem('cart', JSON.stringify(state))
+      return updateCart(state)
     },
   },
 })
