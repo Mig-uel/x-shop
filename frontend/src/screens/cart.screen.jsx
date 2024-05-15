a259bb27115a2087b40ff0e5a7bab0f6be8fcee0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-14 20:04:14 -0400

    refactor: Improve CartScreen component for managing the cart functionality

--- frontend/src/screens/cart.screen.jsx
+++ frontend/src/screens/cart.screen.jsx
@@ -1,5 +1,95 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart } from '../store/cart/cartSlice'
+
+// UI Components
+import { FaTrash } from 'react-icons/fa'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/message.component'
+
 const CartScreen = () => {
-  return <></>
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { cartItems } = useSelector(({ cart }) => cart)
+  const totalItems = cartItems.reduce((acc, item) => acc + item.qty, 0)
+  const subtotal = cartItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
+  )
+
+  const addToCartHandler = (item, value) =>
+    dispatch(addToCart({ ...item, qty: value }))
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: '20px' }}>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        addToCartHandler(item, Number(e.target.value))
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((i) => (
+                        <option key={i + 1} value={i + 1}>
+                          {i + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button type='button' variant='light'>
+                      <FaTrash />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Subtotal ({totalItems}) Items</h2>${subtotal.toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  onClick={() => navigate('/checkout')}
+                  disabled={cartItems.length === 0}
+                >
+                  Proceed to Checkout
+                </Button>
+              </Row>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
 
 export default CartScreen
