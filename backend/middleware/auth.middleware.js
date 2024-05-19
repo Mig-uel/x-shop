e2a4835281617a0d99be612935e3bac1ce2fde95
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-18 22:39:59 -0400

    refactor: Add authentication middleware for protecting routes

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('./asyncHandler.middleware')
+const User = require('../models/user.model')
+
+/** ------ PROTECT ROUTES ------ */
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+  token = req.cookies.jwt // access/read the JWT cookie
+
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET) // decode token
+
+      // assign to req object an object 'user' and value of the User object from db
+      req.user = await User.findById(decoded.userId).select('-password')
+      next()
+    } catch (e) {
+      res.status(401)
+      throw new Error('Not authorized, token failed.')
+    }
+  } else {
+    res.status(401)
+    throw new Error('Not authorized, no token.')
+  }
+})
+
+module.exports = { protect }
