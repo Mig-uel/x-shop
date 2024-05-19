b23ac7d389239d86b425721b8387c03482622237
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-18 21:52:36 -0400

    refactor: Add bcrypt password comparison method to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema(
   {
@@ -24,5 +25,15 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 )
+
+/**
+ * Compares the entered password with the user's password.
+ * @method
+ * @param {string} enteredPassword - The password entered by the user.
+ * @returns {Promise<boolean>} - A promise that resolves to true if the entered password matches the user's password, otherwise false.
+ */
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 
 module.exports = mongoose.model('User', userSchema)
