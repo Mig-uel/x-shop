b46d4843a19beff601f98b2fc61bd3301b4d7bc8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-18 22:45:35 -0400

    refactor: Add admin middleware for protecting admin routes

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -25,4 +25,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
-module.exports = { protect }
+/** ------ PROTECT ADMIN ROUTES ------ */
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as admin.')
+  }
+}
+
