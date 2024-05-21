0c98ec7075c149fed69bba3ce3e99a64b7f719f6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-20 22:05:10 -0400

    refactor: Add auth slice to handle user authentication in store.js

---
+++ frontend/src/store/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit/react'
+
+const initialState = {
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+
+  reducers: {
+    setCredentials: (state, { payload }) => {
+      state.userInfo = payload
+      localStorage.setItem('userInfo', JSON.stringify(payload))
+    },
+  },
+})
+
+export const { setCredentials } = authSlice.actions
+export default authSlice.reducer
