9462e134a07e7eb707aaca04a6cbb1d68cbd8b5b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-20 22:04:57 -0400

    refactor: Add auth slice to store.js for handling user authentication

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
-
-// API Slice
+/** API Slice */
 import { apiSlice } from './api/apiSlice'
-
-// Cart Slice
+/** Cart Slice */
 import cartSliceReducer from './cart/cartSlice'
+/** Auth Slice */
+import authSliceReducer from './auth/authSlice'
 
 export default configureStore({
   reducer: {
     cart: cartSliceReducer,
+    auth: authSliceReducer,
     // api reducer
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
