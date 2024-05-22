6434aba53509c5242e41d7071d7cbd4dc3565371
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-22 01:55:14 -0400

    feat: Add order controller for handling order-related API routes

---
+++ backend/controllers/order.controller.js
@@ -0,0 +1,67 @@
+const Order = require('../models/order.model')
+const asyncHandler = require('../middleware/asyncHandler.middleware')
+
+/** ------ USER ROUTES ------ */
+/**
+ * @desc    Create new order
+ * @route   POST /api/orders
+ * @access  Private
+ */
+const addOrderItems = asyncHandler(async (req, res) => {
+  res.send('add order item')
+})
+
+/**
+ * @desc    Get users orders
+ * @route   GET /api/orders/myorders
+ * @access  Private
+ */
+const getUserOrders = asyncHandler(async (req, res) => {
+  res.send('get user orders')
+})
+
+/**
+ * @desc    Get order by ID
+ * @route   GET /api/orders/:id
+ * @access  Private
+ */
+const getOrderById = asyncHandler(async (req, res) => {
+  res.send('get order by id')
+})
+
+/**
+ * @desc    Update order to paid
+ * @route   PATCH /api/orders/:id/pay
+ * @access  Private
+ */
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  res.send('update order to paid')
+})
+
+/** ------ ADMIN ROUTES ------ */
+/**
+ * @desc    Update order to delivered
+ * @route   PATCH /api/orders/:id/deliver
+ * @access  Private/Admin
+ */
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  res.send('update order to delivered')
+})
+
+/**
+ * @desc    Get all orders
+ * @route   GET /api/orders
+ * @access  Private/Admin
+ */
+const getAllOrders = asyncHandler(async (req, res) => {
+  res.send('get all orders')
+})
+
+module.exports = {
+  addOrderItems,
+  getUserOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getAllOrders,
+}
