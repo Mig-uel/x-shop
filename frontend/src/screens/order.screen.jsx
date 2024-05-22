21744a13755302cd2819ac0eb8019d31217ddc0c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-22 09:36:36 -0400

    feat: Add OrderScreen component for displaying order details

---
+++ frontend/src/screens/order.screen.jsx
@@ -0,0 +1,13 @@
+import { Link, useParams } from 'react-router-dom'
+import { useGetOrderDetailsQuery } from '../store/api/ordersEndpoints.api'
+
+/** UI Elements */
+import Message from '../components/message.component'
+import Loader from '../components/loader.component'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+
+const OrderScreen = () => {
+  return <>OrderScreen</>
+}
+
+export default OrderScreen
