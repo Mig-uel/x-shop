0af55d75860dd9d840861712060b1b3d587fdef3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-23 16:44:52 -0400

    feat: Update updateOrderToDelivered controller to mark order as delivered

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -116,7 +116,21 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
  * @access  Private/Admin
  */
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send('update order to delivered')
+  const { id } = req.params
+
+  const order = await Order.findById(id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.status(200).json(updatedOrder)
+  } else {
+    res.status(400)
+    throw new Error('Order not found')
+  }
 })
 
 /**
