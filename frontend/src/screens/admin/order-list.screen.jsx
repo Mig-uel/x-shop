fe561a761212907a6038917cb24d5c78f2b2301d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-23 16:06:02 -0400

    feat: Update OrderListScreen with Redux integration for fetching orders

--- frontend/src/screens/admin/order-list.screen.jsx
+++ frontend/src/screens/admin/order-list.screen.jsx
@@ -1,3 +1,13 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useGetAllOrdersQuery } from '../../store/api/ordersEndpoints.api'
+import { LinkContainer } from 'react-router-bootstrap'
+
+/** UI Elements */
+import { FaTimes } from 'react-icons/fa'
+import { Table, Button } from 'react-bootstrap'
+import Message from '../../components/message.component'
+import Loader from '../../components/loader.component'
+
 const OrderListScreen = () => {
   return <></>
 }
