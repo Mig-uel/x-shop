d12ade49a57c9c49633ada2b467d6fc860523198
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-23 17:55:51 -0400

    feat: Add admin route to create a new product

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,13 +1,21 @@
 /** Initialize Express Router */
 const router = require('express').Router()
 
+/** Middleware */
+const { protect, admin } = require('../middleware/auth.middleware')
+
 /** Product Controllers */
 const {
   getProducts,
   getSingleProduct,
+  createProduct,
 } = require('../controllers/product.controller')
 
+/** USER ROUTES */
 router.route('/').get(getProducts)
 router.route('/:id').get(getSingleProduct)
+
+/** ADMIN ROUTES */
+router.route('/').post(protect, admin, createProduct)
 
 module.exports = router
