b0e6e1f55984a7725bd3ed0841f44b52faae0d85
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-30 18:11:14 -0400

    feat: Add UserEditScreen component to admin user routes

---
+++ frontend/src/screens/admin/user-edit.screen.jsx
@@ -0,0 +1,187 @@
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import {
+  useGetProductByIdQuery,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+} from '../../store/api/productsEndpoints.api'
+
+/** UI Elements */
+import { toast } from 'react-toastify'
+import Message from '../../components/message.component'
+import Loader from '../../components/loader.component'
+import FormContainer from '../../components/form-container.component'
+import { Form, Button, InputGroup } from 'react-bootstrap'
+
+const UserEditScreen = () => {
+  const { id: productId } = useParams()
+  const navigate = useNavigate()
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const [image, setImage] = useState('')
+  const [brand, setBrand] = useState('')
+  const [category, setCategory] = useState('')
+  const [countInStock, setCountInStock] = useState(0)
+  const [description, setDescription] = useState('')
+
+  const { data: product, isLoading, error } = useGetProductByIdQuery(productId)
+
+  const [updateProduct, { isLoading: isLoadingUpdateProduct }] =
+    useUpdateProductMutation()
+  const [uploadProductImage, { isLoading: isLoadingUploadProductImage }] =
+    useUploadProductImageMutation()
+
+  useEffect(() => {
+    if (product) {
+      setName(product.name)
+      setPrice(product.price)
+      setImage(product.image)
+      setBrand(product.brand)
+      setCategory(product.category || '')
+      setCountInStock(product.countInStock)
+      setDescription(product.description)
+    }
+  }, [product])
+
+  const updateProductHandler = async (e) => {
+    e.preventDefault()
+    const updatedProduct = {
+      productId,
+      name,
+      price,
+      image,
+      brand,
+      category,
+      countInStock,
+      description,
+    }
+
+    const res = await updateProduct(updatedProduct)
+    if (res.error) toast.error(res.error)
+    else {
+      toast.success('Product updated')
+      navigate('/admin/productlist')
+    }
+  }
+
+  const uploadImageHandler = async (e) => {
+    const formData = new FormData()
+    formData.append('image', e.target.files[0])
+
+    try {
+      const res = await uploadProductImage(formData).unwrap()
+      toast.success(res.message)
+      setImage(res.image)
+    } catch (error) {
+      toast.error(error?.data?.message || error?.error)
+    }
+  }
+
+  return (
+    <>
+      <Link to='/admin/userlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+
+      <FormContainer>
+        <h1>Edit Product</h1>
+
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>
+            {error?.data?.message || error?.error}
+          </Message>
+        ) : (
+          <Form onSubmit={updateProductHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='price' className='my-2'>
+              <Form.Label>Price</Form.Label>
+              <InputGroup>
+                <InputGroup.Text id='basic-addon1'>$</InputGroup.Text>
+                <Form.Control
+                  type='number'
+                  placeholder='Enter price'
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </InputGroup>
+            </Form.Group>
+            <Form.Group controlId='image' className='my-2'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                readOnly
+                type='text'
+                placeholder='Enter image url'
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              />
+              <Form.Control type='file' onChange={uploadImageHandler} />
+            </Form.Group>
+            <Form.Group controlId='brand' className='my-2'>
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter brand'
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='countInStock' className='my-2'>
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Enter count in stock'
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='category' className='my-2'>
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter category'
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='description' className='my-2'>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as='textarea'
+                rows={3}
+                placeholder='Enter description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button
+              type='submit'
+              variant='primary'
+              className='my-2'
+              disabled={isLoadingUpdateProduct}
+            >
+              {isLoadingUpdateProduct ? (
+                <Loader width='25px' height='25px' />
+              ) : (
+                'Update'
+              )}
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  )
+}
+
+export default UserEditScreen
