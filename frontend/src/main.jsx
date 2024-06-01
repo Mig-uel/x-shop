c4f6b91e6b1d79cbe4590be604bc95d850b18ade
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-01 18:42:55 -0400

    feat: Remove redundant route for HomeScreen pagination

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -40,7 +40,6 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index element={<HomeScreen />} />
-      <Route path='/page/:pageNumber' element={<HomeScreen />} />
       <Route path='product/:id' element={<ProductScreen />} />
       <Route path='cart' element={<CartScreen />} />
       <Route path='login' element={<LoginScreen />} />
