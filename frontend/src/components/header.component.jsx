be78b39b23513e2b36f9ce1f8a68017731de6e7a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-01 19:41:44 -0400

    feat: Add search box component to header

--- frontend/src/components/header.component.jsx
+++ frontend/src/components/header.component.jsx
@@ -5,6 +5,7 @@ import { removeCredentials } from '../store/auth/authSlice'
 import { useLogoutMutation } from '../store/api/usersEndpoints.api'
 
 // UI Components
+import SearchBox from './search-box.component'
 import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../assets/logo.png'
@@ -40,6 +41,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
+              <SearchBox />
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
