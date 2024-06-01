21114d9ebd4a974666c302d953f035e949c8a582
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-01 18:44:00 -0400

    feat: Refactor Paginate component to use react-router-dom for navigation

--- frontend/src/components/paginate.component.jsx
+++ frontend/src/components/paginate.component.jsx
@@ -1,17 +1,35 @@
 import { Pagination } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import { useSearchParams, useNavigate, useParams } from 'react-router-dom'
 
 const Paginate = ({ pages, page, isAdmin = false }) => {
+  const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const searchParamsHandler = (path, p) => {
+    setSearchParams('pageNumber', p)
+    navigate({
+      pathname: path,
+      search: `?pageNumber=${p}`,
+    })
+  }
+
   return (
     pages > 1 && (
       <Pagination>
         {[...Array(pages).keys()].map((p) => (
-          <LinkContainer
-            key={p + 1}
-            to={!isAdmin ? `/page/${p + 1}` : `/admin/productlist/${p + 1}`}
-          >
-            <Pagination.Item active={p + 1 === page}>{p + 1}</Pagination.Item>
-          </LinkContainer>
+          <div key={p + 1}>
+            <Pagination.Item
+              active={p + 1 === page}
+              onClick={() =>
+                searchParamsHandler(
+                  !isAdmin ? `/` : `/admin/productlist/`,
+                  p + 1
+                )
+              }
+            >
+              {p + 1}
+            </Pagination.Item>
+          </div>
         ))}
       </Pagination>
     )
