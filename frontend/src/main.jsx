50a83d263febf5c930655639b1dab2fb273f2c53
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-01 20:54:38 -0400

    feat: Add react-helmet-async package and wrap the app with HelmetProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 
 /** Global State */
@@ -67,10 +68,12 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PayPalScriptProvider deferLoading={true}>
-        <RouterProvider router={router} />
-      </PayPalScriptProvider>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <PayPalScriptProvider deferLoading={true}>
+          <RouterProvider router={router} />
+        </PayPalScriptProvider>
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 )
